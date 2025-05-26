efe3d0ab63c79d737e12e008115d3d93649737f1
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-05-27 05:18:00 +0900

    In the middle of experiment: specify key_prefix in load.sh and run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,15 @@ fi
 NUM_INSTANCES=$1
 BASE_PORT=11211
 WORKLOAD_FILE="workloads/myworkload"
+HOST_ID=$(hostname)
 
 for ((i = 0; i < NUM_INSTANCES; i++)); do
 	PORT=$((BASE_PORT + i))
+	PREFIX="${HOST_ID}_instance${i}_"
 	echo "Running workload against memcached on port $PORT"
 	./bin/ycsb run memcached -s -P "$WORKLOAD_FILE" \
 		-p "memcached.hosts=127.0.0.1:$PORT" \
+		-p "insert.key_prefix=$PREFIX" \
 		-threads $(nproc) \
 		>"LOG/output-${PORT}-Run.txt" &
 done
